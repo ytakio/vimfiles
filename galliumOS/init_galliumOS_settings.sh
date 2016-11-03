6fd8b6fa0bb85c2ba0a02eb3093ee6060724bcee
Takio <y.takio@gmail.com>
2016-11-03 12:40:54 +0900

    Update for backup

--- galliumOS/init_galliumOS_settings.sh
+++ galliumOS/init_galliumOS_settings.sh
@@ -1,13 +1,25 @@
-#!/bin/bash -x
-$CONFIG=/var/config
-if [ -n "$SUDO_USER" ]; then
-	USER=$SUDO_USER
+#!/bin/bash
+ECHO=echo
+if [ -n "${SUDO_USER}" ]; then
+	USER=${SUDO_USER}
+else
+	echo "sudo init_galliumOS_settings.sh"
+	exit 1
 fi
 
-echo "create $CONFIG for user config directory"
-mkdir -p $CONFIG
-chmod a+rw $CONFIG
+mirror_and_link() {
+	NUMBER=$1
+	TARGET=$2
+	MIRROR=$3
+	echo "${NUMBER}. mirroring ${TARGET} to ${MIRROR}"
+	echo "${NUMBER}.1. create ${MIRROR} for user config directory"
+	$ECHO mkdir -p ${MIRROR}
+	$ECHO rmdir -rf ${MIRROR}
 
-echo "move $HOME/$name to $MIRROR/$USER/$name"
-mv $HOME/.config $CONFIG/$USER
-ln -nfs $CONFIG/$USER $HOME/.config
+	echo "${NUMBER}.2. move and link ${HOME}/${TARGET} to ${MIRROR}"
+	$ECHO mv ${HOME}/${TARGET} ${MIRROR}
+	$ECHO ln -nfs ${MIRROR} ${HOME}/${TARGET}
+}
+
+mirror_and_link 1 .config /var/config/${USER}
+mirror_and_link 2 .cache /cache_${USER}
