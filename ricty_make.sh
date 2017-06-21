69438d5123f0d7504920ae11f0ead349fb831321
Takio <y.takio@gmail.com>
2017-06-22 08:49:32 +0900

    M1 フォントのパス変更

--- ricty_make.sh
+++ ricty_make.sh
@@ -5,9 +5,9 @@ cd Ricty
 wget http://www.rs.tus.ac.jp/yyusa/ricty/ricty_generator.sh; chmod a+x ricty_generator.sh
 wget --content-disposition https://github.com/google/fonts/blob/master/ofl/inconsolata/Inconsolata-Bold.ttf?raw=true
 wget --content-disposition https://github.com/google/fonts/blob/master/ofl/inconsolata/Inconsolata-Regular.ttf?raw=true
-wget http://iij.dl.sourceforge.jp/mix-mplus-ipa/59022/migu-1m-20130617.zip
-unzip migu-1m-20130617.zip
-mv migu-1m-20130617/* ./
+wget http://osdn.dl.osdn.jp/mix-mplus-ipa/63545/migu-1m-20150712.zip
+unzip migu-1m-20150712.zip
+mv migu-1m-20150712/* ./
 
 ./ricty_generator.sh auto
 
